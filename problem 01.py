92a0721342d5e36d27db98918ac1208cfb775912
preanto <preanto15-5059@diu.edu.bd>
2024-11-04 22:52:47 +0600

    problem 01 - python masterclass

---
+++ problem 01.py
@@ -0,0 +1,11 @@
+# Problem1:
+# Write a Python program that takes a string input from the user and checks if Enjoy available in
+# the input string:
+# "Hello, world! Hello everyone. Welcome to the world of Python. Enjoy coding in Python."
+
+a = input("Please input a string: ")
+if 'Enjoy' in a:
+    print("Yes, Enjoy is found")
+else:
+    print("Not found")
+    
